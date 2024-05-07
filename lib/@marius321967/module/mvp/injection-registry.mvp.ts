0ef654e963c777edc44daa9cdf50afbdaae98ecd
marius321967 <marius.dras@gmail.com>
2024-05-07 19:48:01 +0000

    injection registry mvp

---
+++ lib/@marius321967/module/mvp/injection-registry.mvp.ts
@@ -0,0 +1,40 @@
+import {
+  Builder,
+  BuilderRegistrator,
+  Token,
+  ValueRegistrator,
+} from './di-types';
+
+const collection: { [key: symbol]: Builder<any> } = {};
+
+export const registerBuilder: BuilderRegistrator = <T>(
+  builder: Builder<T>,
+  token?: Token<T>,
+): Token<T> => {
+  if (!token) token = Symbol();
+  collection[token as symbol] = builder;
+
+  return token;
+};
+
+export const registerValue: ValueRegistrator = <T>(
+  value: T,
+  token?: Token<T>,
+): Token<T> => {
+  if (!token) token = Symbol();
+  collection[token as symbol] = () => value;
+
+  return token;
+};
+
+export const inject = <T>(token: Token<T>): T => {
+  if (!((token as symbol) in collection)) {
+    const tokenName = token
+      .toString()
+      .substring(7, token.toString().length - 1);
+
+    throw new Error(`DI container does not have a value for [${tokenName}]`);
+  }
+
+  return collection[token as symbol](inject);
+};
