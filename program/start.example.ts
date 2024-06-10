448a417bd34a7a2bf37967de555e3bf952194f82
marius321967 <marius.dras@gmail.com>
2024-06-10 19:31:42 +0000

    move to program/

---
+++ program/start.example.ts
@@ -0,0 +1,4 @@
+import { myName1, mypass1 } from './my-values';
+import testProgram from './test-program';
+
+testProgram(mypass1, myName1);
--- start.example.ts
+++
@@ -1,3 +0,0 @@
-import testProgram, { mypass1 } from './test-program';
-
-testProgram(mypass1);
