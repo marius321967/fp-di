033e59356d23dbc8468b2cc91530e577d69b3feb
marius321967 <marius.dras@gmail.com>
2024-09-26 20:01:30 +0000

    add 'export {x}' case in sample program

--- program/my-values.ts
+++ program/my-values.ts
@@ -2,3 +2,7 @@ import { MyName, MyPassword } from './my-types';
 
 export const mypass1: MyPassword = 'foo!',
   myName1: MyName = 'marius';
+
+const myName2: MyName = 'john';
+
+export { myName2 };
