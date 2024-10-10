16a4dbe38f234317446dac2466e7d28eefe49057
marius321967 <marius.dras@gmail.com>
2024-10-10 19:37:56 +0000

    refactor: reuse fn instead of duplicating

--- lib/@marius321967/module/lib/generator/symbols.ts
+++ lib/@marius321967/module/lib/generator/symbols.ts
@@ -1,28 +1,13 @@
 import ts from 'typescript';
 import { getSymbolAtLocation, resolveOriginalSymbol } from '../helpers/symbols';
-
-const extractTypeReferences = (
-  typeNode: ts.TypeNode,
-): ts.TypeReferenceNode[] => {
-  if (ts.isTypeReferenceNode(typeNode)) {
-    return [typeNode];
-  }
-
-  if (ts.isUnionTypeNode(typeNode)) {
-    return typeNode.types.filter(ts.isTypeReferenceNode);
-  }
-
-  throw new Error(
-    `Only TypeReferenceNode, UnionType supported [${typeNode.getText()}]`,
-  );
-};
+import { toAcceptedTypes } from './fills/toAcceptedTypes';
 
 /** @returns Single symbol in case of TypeReferenceNode, multiple possible symbols in case of UnionTypeNode */
 export const resolveTypeNodeSymbols = (
   typeNode: ts.TypeNode,
   typeChecker: ts.TypeChecker,
 ): ts.Symbol[] => {
-  const typeReferences = extractTypeReferences(typeNode);
+  const typeReferences = toAcceptedTypes(typeNode);
 
   return typeReferences.map((typeReference) => {
     const symbol = getSymbolAtLocation(typeReference.typeName, typeChecker);
