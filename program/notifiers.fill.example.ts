5bdd3f866468ef6e3f1f980ea49e5cc8d3a88341
marius321967 <marius.dras@gmail.com>
2024-10-29 19:13:09 +0000

    chore: reconfigure sample tsconfig to avoid parsing examples

--- program/notifiers.fill.example.ts
+++ program/notifiers.fill.example.ts
@@ -1,4 +1,4 @@
 import { Notifier } from './my-types';
 import { smsNotifier } from './notifiers';
-import { fill_buildFakeSmsService } from './sms-service.fill.expected';
+import { fill_buildFakeSmsService } from './sms-service.fill.example';
 export const fill_smsNotifier: Notifier = smsNotifier(fill_buildFakeSmsService);
--- program/notifyUser.fill.example.ts
+++ program/notifyUser.fill.example.ts
@@ -1,4 +1,4 @@
 import { UserNotifier } from './my-types';
-import { fill_smsNotifier } from './notifiers.fill.expected';
+import { fill_smsNotifier } from './notifiers.fill.example';
 import { notifyUser } from './notifyUser';
 export const fill_notifyUser: UserNotifier = notifyUser(fill_smsNotifier);
