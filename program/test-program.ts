7a2739d5ba5a46a7774cb0c781df4a91b3008d88
marius321967 <marius.dras@gmail.com>
2024-11-12 19:18:11 +0000

    test: sample program to use 3rd level recursive fills

--- program/test-program.ts
+++ program/test-program.ts
@@ -1,4 +1,4 @@
-import { MyEmail, MyName, MyPassword, Notifier } from './my-types';
+import { MyEmail, MyName, MyPassword, UserNotifier } from './my-types';
 import './my-values';
 import './notifiers';
 import './notifyUser';
@@ -15,8 +15,12 @@ import './userDeleters';
 // - declaration must be exported either as default or const
 // - there must not be competing candidates
 
-export default (pass: MyPassword, name: MyEmail | MyName, notify: Notifier) => {
+export default (
+  pass: MyPassword,
+  name: MyEmail | MyName,
+  notifyUser: UserNotifier,
+) => {
   console.log('the program runs with values:', pass, name);
 
-  notify(`${name}:${pass}`);
+  notifyUser({ contact: name, id: '#123', password: pass });
 };
