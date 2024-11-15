89b8eaa4cbdef05212b5313bc87770f4b5b7e6ce
marius321967 <marius.dras@gmail.com>
2024-11-15 18:19:29 +0000

    test: remove comments in sample

--- samples/recursive-fills/test-program.ts
+++ samples/recursive-fills/test-program.ts
@@ -5,16 +5,6 @@ import './notifyUser';
 import './sms-config';
 import './userDeleters';
 
-// for function to be injected:
-// - function type must be exported
-// - builder must identify return type (which is type above)
-// - builder must be exported
-
-// for var to be injected:
-// - type must be exported
-// - declaration must be exported either as default or const
-// - there must not be competing candidates
-
 export default (
   pass: MyPassword,
   name: MyEmail | MyName,
