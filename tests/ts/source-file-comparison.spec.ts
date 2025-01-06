646ad0b7a4ae7a7fe9543e0740755c31555b95f2
marius321967 <marius.dras@gmail.com>
2025-01-06 19:49:54 +0000

    test: ensure typescript getSourceFile() returns same object

--- tests/ts/source-file-comparison.spec.ts
+++ tests/ts/source-file-comparison.spec.ts
@@ -1,3 +1,48 @@
+import { assert, expect } from 'chai';
+import { dirname } from 'path';
+import { createProgram } from 'typescript';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 describe('node.getSourceFile() should return the exact same object for same file, not a copy of the SourceFile object', () => {
-  // ...
+  const program = createProgram(
+    [__dirname + '/sample-program.ts', __dirname + '/sample-program-2.ts'],
+    {},
+  );
+  program.getTypeChecker();
+
+  it('main test', () => {
+    const sourceFile = program.getSourceFile(__dirname + '/sample-program.ts');
+
+    if (!sourceFile) {
+      assert.fail('Could not parse sample program for test');
+    }
+
+    const nodes = sourceFile.getChildren()[0].getChildren();
+
+    expect(nodes.length).to.equal(2);
+    expect(nodes[0].getText()).to.equal('const x = 1;');
+    expect(nodes[1].getText()).to.equal('const y = 2;');
+
+    expect(nodes[0].getSourceFile() === nodes[1].getSourceFile()).to.equal(
+      true,
+    );
+  });
+
+  it('nodes from different files should return different getSourceFile (just in case)', () => {
+    const sourceFile1 = program.getSourceFile(__dirname + '/sample-program.ts');
+    const sourceFile2 = program.getSourceFile(
+      __dirname + '/sample-program-2.ts',
+    );
+
+    if (!sourceFile1 || !sourceFile2) {
+      assert.fail('Could not parse sample program for test');
+    }
+
+    expect(
+      sourceFile1.getChildren()[0].getSourceFile() ===
+        sourceFile2.getChildren()[0].getSourceFile(),
+    ).to.equal(false);
+  });
 });
