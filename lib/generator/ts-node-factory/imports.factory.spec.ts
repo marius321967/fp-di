fc22f53d562ae37d4948607d913d9dae93140e24
marius321967 <marius.dras@gmail.com>
2025-02-04 20:22:56 +0000

    test: initializing unit tests

---
+++ lib/generator/ts-node-factory/imports.factory.spec.ts
@@ -0,0 +1,35 @@
+import { assert, expect } from 'chai';
+import ts from 'typescript';
+import { ExportAs } from '../../types.js';
+import { createImportClauseFromContext } from './imports.factory.js';
+
+describe('createImportClauseFromContext', () => {
+  it('should return default import clause when target exported as default', () => {
+    const importAs = 'foo';
+    const exportedAs: ExportAs = { type: 'default' };
+
+    const result = createImportClauseFromContext({ importAs, exportedAs });
+
+    expect(result.namedBindings).to.be.undefined;
+    expect(result.name).to.deep.equal(ts.factory.createIdentifier(importAs));
+  });
+
+  it('should return simple named import when target exported with name', () => {
+    const importAs = 'foo';
+    const exportedAs: ExportAs = { type: 'named', name: 'foo' };
+
+    const result = createImportClauseFromContext({ importAs, exportedAs });
+
+    assert.isDefined(result.namedBindings);
+    // TODO use forEachChild
+    const importSpecifier = result.namedBindings.getChildAt(
+      0,
+    ) as ts.ImportSpecifier;
+    expect(importSpecifier.name.escapedText).to.equal('foo');
+    expect(importSpecifier.propertyName).to.be.undefined;
+  });
+
+  it('should import under different name when import name does not match target export name', () => {
+    // TODO
+  });
+});
