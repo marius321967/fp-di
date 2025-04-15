f2fa842919abacfcb59aaac5133f31f146bb9167
marius321967 <marius.dras@gmail.com>
2025-04-15 19:07:52 +0000

    ci: solve ts-node issue on node v20 and up

--- tests/tools/run.ts
+++ tests/tools/run.ts
@@ -1,8 +1,12 @@
 import { exec } from 'child_process';
 
+const nodeMajorVersion = parseInt(process.versions.node.split('.')[0]);
+
 export const run = (path: string): Promise<string> =>
   new Promise((resolve, reject) => {
-    exec(`ts-node-esm ${path}`, (error, stdout) => {
+    const tsNodeLoadOption = nodeMajorVersion >= 20 ? 'require' : 'loader';
+
+    exec(`node --${tsNodeLoadOption}=ts-node/esm ${path}`, (error, stdout) => {
       if (error) {
         reject(error);
       }
