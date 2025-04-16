e1fbcdafd8ed29fcf8b9c7687cf88d4404a379d6
marius321967 <marius.dras@gmail.com>
2025-04-16 19:42:56 +0000

    ci: drop ts-node for tsx

--- tests/tools/run.ts
+++ tests/tools/run.ts
@@ -1,12 +1,8 @@
 import { exec } from 'child_process';
 
-const nodeMajorVersion = parseInt(process.versions.node.split('.')[0]);
-
 export const run = (path: string): Promise<string> =>
   new Promise((resolve, reject) => {
-    const tsNodeLoadOption = nodeMajorVersion >= 20 ? 'require' : 'loader';
-
-    exec(`node --${tsNodeLoadOption}=ts-node/esm ${path}`, (error, stdout) => {
+    exec(`node_modules/.bin/tsx ${path}`, (error, stdout) => {
       if (error) {
         reject(error);
       }
